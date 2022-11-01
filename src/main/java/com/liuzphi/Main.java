3aa90e108dbfd9b31321df989aea711cf5dad2ab
“liuzipeng” <liuzphi@163.com>
2022-11-01 10:24:36 +0800

    aa

---
+++ src/main/java/com/liuzphi/Main.java
@@ -0,0 +1,7 @@
+package com.liuzphi;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/liuzphi/test1101/Main704.java
@@ -0,0 +1,7 @@
+package com.liuzphi.test1101;
+
+public class Main704 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
